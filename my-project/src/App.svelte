4a607abc670baec76b1183e74e412cb3a54499cb
rajdeepch10 <rajdeepch0@gmail.com>
2022-02-20 22:35:00 +0530

    Added Counter project

---
+++ my-project/src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+  import MyComponent from "./MyComponent.svelte";
+  export let name;
+  let displayWarning = false;
+  const props = {
+    name: "Rajdeep",
+    address: "Hyderabad",
+    counter: 0,
+  };
+  let setCounter = (e) => {
+    if (!isNaN(e.target.value) && e.target.value.length > 0) {
+      console.log("Number");
+      props.counter = parseInt(e.target.value);
+      displayWarning = false;
+    } else if (e.target.value.length === 0) {
+      console.log("Not Number");
+      props.counter = 0;
+      displayWarning = false;
+    } else {
+      props.counter = 0;
+      displayWarning = true;
+    }
+  };
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Enter Initial Count below:</p>
+  <input type="text" name="" on:input={(e) => setCounter(e)} />
+  <p
+    id="warningMessage"
+    style={displayWarning ? "display:block" : "display:none"}
+  >
+    Enter valid number
+  </p>
+  <MyComponent {...props} />
+</main>
+
+<style>
+  #warningMessage {
+    color: red;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ my-project/src/MyComponent.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let name;
+  export let address;
+  export let counter;
+
+  function incrementCounter() {
+    counter++;
+  }
+  function decrementCounter() {
+    counter--;
+  }
+</script>
+
+<p>My name is {name} and I live in {address}</p>
+<button on:click={incrementCounter}>Increment Counter</button>
+<button on:click={decrementCounter}>Decrement Counter</button>
+<p>{counter}</p>
+
+<style>
+  button {
+    color: blueviolet;
+    height: 60px;
+    width: 100px;
+  }
+</style>
